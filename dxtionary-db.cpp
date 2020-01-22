0efe2d851e66cfc5bc26d4c095325596d5d7fe86
Hong-Phuc Bui <hong-phuc.bui@htwsaar.de>
2020-01-22 16:51:22 +0100

    use cpp as CLI program, improve CMakeList.txt

---
+++ dxtionary-db.cpp
@@ -0,0 +1,64 @@
+/** 
+ * provide CLI to query/insert data to dictionary database
+ *
+ * */
+#include <iostream>
+#include <sqlite3.h>
+
+#include "dxtionary_version.h"
+
+
+using namespace std;
+
+int sqlite3_spellfix_init(
+  sqlite3 *db,
+  char **pzErrMsg,
+  const sqlite3_api_routines *pApi
+);
+
+static int callback(void *NotUsed, int argc, char **argv, char **azColName)
+{
+
+    for(int i = 0; i < argc; i++){
+        cout << azColName[i] << " = " << (argv[i] ? argv[i] : "NULL") << endl;
+    }    
+    cout << endl;
+    return 0;
+}
+
+int main(int argc, const char* argv[]) 
+{
+    if (argc != 3) {
+		// report version        
+        cout << argv[0] << " Version "
+             << dxtionary_VERSION_MAJOR << "."
+             << dxtionary_VERSION_MINOR << "."
+             << dxtionary_VERSION_PATCH;
+        cout << "Usage: " << argv[0] << " DATABASE SQL-STATEMENT";
+		return 1;
+	}
+
+    sqlite3 *db;
+    int openDbOk = sqlite3_open(argv[1], &db);
+    if (openDbOk != 0)
+    {
+
+        cerr << "Can't open database: " << sqlite3_errmsg(db) << endl;
+        sqlite3_close(db);
+        return 1;
+    }
+
+    char *zErrMsg = nullptr;
+    int excuteSttmOK = sqlite3_exec(db, argv[2], callback, nullptr, &zErrMsg);
+    if (excuteSttmOK != 0)
+    {
+        cerr << "SQL error: " << zErrMsg << endl;
+        sqlite3_free(zErrMsg);
+    }
+    sqlite3_close(db);
+	
+	return 0;
+}
+
+
+
--- sqlite3/extra_init.in.c
+++ sqlite3/extra_init.in.c
@@ -1,4 +1,4 @@
-int core_init(const char* dummy)
+int extra_init(const char* dummy)
 {
         int nErr = 0;
         nErr += sqlite3_auto_extension((void (*)(void))sqlite3_spellfix_init);
