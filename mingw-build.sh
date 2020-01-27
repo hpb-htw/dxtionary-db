a588ae2bfd0775a547136593d078ad0d1d14496f
Hong-Phuc Bui <hong-phuc.bui@htwsaar.de>
2020-01-27 23:36:18 +0100

    disable load extension of sqlite

--- mingw-build.sh
+++ mingw-build.sh
@@ -8,4 +8,4 @@ export CC=`which x86_64-w64-mingw32-gcc`
 # export CXX=`which i686-w64-mingw32-g++`
 export CXX=`which x86_64-w64-mingw32-g++`
 
-cmake -S . -B cmake-build-mingw
+cmake -S . -B cmake-build-mingw -DSKIP_INSTALL_HEADERS=False
