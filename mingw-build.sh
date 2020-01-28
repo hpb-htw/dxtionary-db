565331c868ad9d4c9a8f6ca5e6539939bcdd4a74
Hong-Phuc Bui <hong-phuc.bui@htwsaar.de>
2020-01-28 12:23:41 +0100

    cross compile may work

--- mingw-build.sh
+++ mingw-build.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
+# NOTE: not work at all, see later if have time
+
+platform="i686-w64-mingw32"
+# alternatives:
+# x86_64-w64-mingw32
+# i686-w64-mingw32
+# i586-mingw32msvc
+# x86_64-w64-mingw32
+
 # C
-# export CC=`which i686-w64-mingw32-gcc`
-export CC=`which x86_64-w64-mingw32-gcc`
+export CC=$(which "${platform}-gcc")
 
 # C++
-# export CXX=`which i686-w64-mingw32-g++`
-export CXX=`which x86_64-w64-mingw32-g++`
+export CXX=$(which "${platform}-g++")
 
-cmake -S . -B cmake-build-mingw -DSKIP_INSTALL_HEADERS=False
+# setup windres
+WINRES=$(which "${platform}-windres")
+
+
+cmake -S . -B cmake-build-mingw -DSKIP_INSTALL_HEADERS=False -DCMAKE_RC_COMPILER=${WINRES}
