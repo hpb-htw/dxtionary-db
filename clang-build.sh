030479a522a92dace141c101ffa69e1523f9989d
Hong-Phuc Bui <hong-phuc.bui@htwsaar.de>
2020-02-25 16:44:22 +0100

    test compile with clang on AppVeyor

--- clang-build.sh
+++ clang-build.sh
@@ -6,7 +6,8 @@
 
 # ---------------------------------------- #
 
-cmake -S . -B clang-build -DCMAKE_TOOLCHAIN_FILE=clang-toolchain.cmake -DCMAKE_BUILD_TYPE=Release
+cmake -S . -B clang-build       -DCMAKE_TOOLCHAIN_FILE=clang-toolchain.cmake -DCMAKE_BUILD_TYPE=Release
 cd clang-build && cmake --build . && cd test && ctest && cd ../..
 # to make debug version:
 cmake -S . -B clang-build-debug -DCMAKE_TOOLCHAIN_FILE=clang-toolchain.cmake -DCMAKE_BUILD_TYPE=Debug
+cd clang-build-debug && cmake --build . && cd test && ctest && cd ../..
