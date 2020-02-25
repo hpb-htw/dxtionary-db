375a03e8db811e2148bf3209a2c9219b05442e06
Hong-Phuc Bui <hong-phuc.bui@htwsaar.de>
2020-02-25 23:19:05 +0100

    test compile with clang on AppVeyor Di 25. Feb 23:19:05 CET 2020

--- install-clang-tools.sh
+++ install-clang-tools.sh
@@ -42,7 +42,6 @@ sudo update-alternatives \
 
 sudo update-alternatives \
   --install /usr/bin/clang                 clang                  /usr/bin/clang-10     20 \
-  --install /usr/bin/clang++               clang++                /usr/bin/clang++-10 20 \
   --slave   /usr/bin/lld                   lld                    /usr/bin/lld-10 \
   --slave   /usr/bin/clang-format          clang-format           /usr/bin/clang-format-10  \
   --slave   /usr/bin/clang-tidy            clang-tidy             /usr/bin/clang-tidy-10  \
@@ -67,3 +66,7 @@ sudo update-alternatives \
   --slave   /usr/bin/find-all-symbols      find-all-symbols       /usr/bin/find-all-symbols-10 \
   --slave   /usr/bin/lldb                  lldb                   /usr/bin/lldb-10 \
   --slave   /usr/bin/lldb-server           lldb-server            /usr/bin/lldb-server-10
+
+sudo update-alternatives \
+  --install /usr/bin/clang++               clang++                /usr/bin/clang++-10 20
+
